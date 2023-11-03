45339aa95d2b5599ff98c157d44476593dd66f20
papriabhuiyan <56096609+papriabhuiyan@users.noreply.github.com>
2023-11-03 03:33:21 -0500

    fully functioning app in react

--- src/App.js
+++ src/App.js
@@ -1,117 +1,4 @@
 
-// import React, { useState } from 'react';
-
-// const App = () => {
-//   const [initialWeight, setInitialWeight] = useState('');
-//   const [initialComposition, setInitialComposition] = useState('');
-//   const [finalComposition, setFinalComposition] = useState('');
-//   const [elementsToAdd, setElementsToAdd] = useState([]);
-
-//   const calculateElementAdditions = (initialWeight, initialComp, finalComp) => {
-//     const elementsToAdd = new Array(initialComp.length).fill(0);
-//     let totalAddedWeight = 0;
-//     let newTotalWeight = initialWeight;
-
-//     for (let i = 0; i < initialComp.length; i++) {
-//       const currentWeight = (initialWeight * initialComp[i]) / 100;
-//       const targetWeight = (newTotalWeight * finalComp[i]) / 100;
-//       const addition = Math.max(0, targetWeight - currentWeight);
-//       elementsToAdd[i] += addition;
-//       totalAddedWeight += addition;
-//     }
-
-//     newTotalWeight += totalAddedWeight;
-
-//     let adjustmentNeeded = true;
-//     while (adjustmentNeeded) {
-//       adjustmentNeeded = false;
-//       for (let i = 0; i < initialComp.length; i++) {
-//         const currentWeight = (initialWeight * initialComp[i]) / 100 + elementsToAdd[i];
-//         const targetWeight = (newTotalWeight * finalComp[i]) / 100;
-//         const addition = targetWeight - currentWeight;
-//         if (addition > 0.01) {
-//           elementsToAdd[i] += addition;
-//           totalAddedWeight += addition;
-//           newTotalWeight += addition;
-//           adjustmentNeeded = true;
-//         }
-//       }
-//     }
-
-//     return elementsToAdd.map(addition => addition.toFixed(2));
-//   };
-
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     const initialCompArray = initialComposition.split(' ').map(Number);
-//     const finalCompArray = finalComposition.split(' ').map(Number);
-//     const additions = calculateElementAdditions(parseFloat(initialWeight), initialCompArray, finalCompArray);
-//     setElementsToAdd(additions);
-//   };
-
-//   return (
-//     <div>
-//       <h1>Composition Adjustment Calculator</h1>
-//       <form onSubmit={handleSubmit}>
-//         <div>
-//           <label>
-//             Initial Weight in grams:
-//             <input
-//               type="number"
-//               value={initialWeight}
-//               onChange={(e) => setInitialWeight(e.target.value)}
-//               required
-//             />
-//           </label>
-//         </div>
-//         <div>
-//           <label>
-//             Initial Composition (%):
-//             <input
-//               type="text"
-//               value={initialComposition}
-//               onChange={(e) => setInitialComposition(e.target.value)}
-//               required
-//               placeholder="20 10 15 ..."
-//             />
-//           </label>
-//         </div>
-//         <div>
-//           <label>
-//             Final Composition (%):
-//             <input
-//               type="text"
-//               value={finalComposition}
-//               onChange={(e) => setFinalComposition(e.target.value)}
-//               required
-//               placeholder="15 15 10 ..."
-//             />
-//           </label>
-//         </div>
-//         <button type="submit">Calculate</button>
-//       </form>
-
-//       {elementsToAdd.length > 0 && (
-//         <div>
-//           <h2>Amount to add for each element (in grams):</h2>
-//           <ul>
-//             {elementsToAdd.map((amount, index) => (
-//               <li key={index}>
-//                 Element {index + 1}: {amount} grams
-//               </li>
-//             ))}
-//           </ul>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default App;
-
-
-
-
 
 import React, { useState } from 'react';
 import './App.css';
@@ -270,23 +157,24 @@ const App = () => {
 
 <div>
   <div className='main-div'>
-  <h1>Composition Adjustment Calculator</h1>
+  <h1 className='text-color'>Composition Adjustment Calculator</h1>
   {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
   <form onSubmit={handleSubmit}>
     <table>
       <thead>
         <tr>
-          <th>Element</th>
-          <th>Initial (%)</th>
-          <th>Final (%)</th>
+          <th className='header-color'>Element</th>
+          <th className='header-color'>Initial (%)</th>
+          <th className='header-color'>Final (%)</th>
         </tr>
       </thead>
       <tbody>
         {elements.map((element, index) => (
           <tr key={element}>
-            <td>{element}</td>
+            <td className='text-color'>{element}</td>
             <td>
               <input
+                className='custom-input'
                 type="number"
                 step="0.001"
                 value={initialComposition[index]}
@@ -298,6 +186,7 @@ const App = () => {
             </td>
             <td>
               <input
+                className='custom-input'
                 type="number"
                 step="0.001"
                 value={finalComposition[index]}
@@ -313,9 +202,10 @@ const App = () => {
     </table>
 
     <div className="initial-weight-div">
-      <label>
+      <label className='text-color'>
         Initial Weight in grams:
         <input
+          className='custom-input'
           type="number"
           step="0.001"
           value={initialWeight}
@@ -327,7 +217,7 @@ const App = () => {
       </label>
     </div>
 
-    <button type="submit">Calculate</button>
+    <button className='custom-button' type="submit">Calculate</button>
   </form>
   </div>
   {/* {elementsToAdd.length > 0 && (
@@ -342,10 +232,10 @@ const App = () => {
       </ul>
     </div>
   )} */}
-  <div className='results'>
+  <div className='results text-color'>
   {elementsToAdd.length > 0 && (
   <div>
-    <h2>Amount to add for each element (in grams):</h2>
+    <h2>Amount to add for each element (in grams):    </h2>
     <ul style={{ listStyleType: 'none', padding: 0 }}>
       {elementsToAdd.map((amount, index) => (
         <li key={index} style={{ display: 'flex', justifyContent: 'space-between' }}>
